31fb00201cd9a7321be63c9b31a5e0ed419da1bb
javascriptsucks <evix1988@live.cn>
2023-01-09 22:56:54 -0600

    reverse-string-js

---
+++ pig-latin.js
@@ -0,0 +1,8 @@
+const args = process.argv.slice(2);
+
+const pigLatin = (arr) => {
+  console.log(arr);
+};
+
+
+pigLatin(args);
--- reverse.js
+++ reverse.js
@@ -2,13 +2,12 @@ const args = process.argv.slice(2);
 
 const reverse = (arr) => {
   arr.forEach(word => {
-    let wordArr = word.split('');
+    const wordArr = word.split('');
     const reverseWord = wordArr.reduceRight((prev,curr) => {
       return prev + curr;
     });
     console.log(reverseWord);
   });
 };
-
 
 reverse(args);
