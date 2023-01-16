7127c47e39204221875d86988ec357c18d9f07f0
javascriptsucks <evix1988@live.cn>
2023-01-16 12:29:15 -0600

    test-eqArray-js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,17 +1,18 @@
 const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(1,  1)); // => true
-console.log(eqArrays(null,  null)); // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
+assertEqual(eqArrays(1,  1),true); // => true
+assertEqual(eqArrays(null,  null),true); // => true
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(eqArrays({a:1,b:3}, {a:2,b:1})); // => false
-console.log(eqArrays({a:1,b:3}, {b:3,a:1})); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
+//assertEqual(eqArrays({a:1,b:3}, {a:2,b:1}),false); // => false
+//assertEqual(eqArrays({a:1,b:3}, {b:3,a:1}),true); // => true
 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => true
-console.log(eqArrays([[2,[[]], 3],[[[[[[[[[[[6]]]]]]]]]]] [4]], [[2,[[]], 3],[[[[[[[[[[[6]]]]]]]]]]] [4]])); // => true
-console.log(eqArrays([[[[2, 3],2,4,[6]], [8,9]], 4], [[[[2, 3],2,4,[6]], [8,9]], 4])); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([[2,[[]], 3],[[[[[[[[[[[6]]]]]]]]]]] [4]], [[2,[[]], 3],[[[[[[[[[[[6]]]]]]]]]]] [4]]),true); // => true
+assertEqual(eqArrays([[[[2, 3],2,4,[6]], [8,9]], 4], [[[[2, 3],2,4,[6]], [8,9]], 4]),true); // => true
